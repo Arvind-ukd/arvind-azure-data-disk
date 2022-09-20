82037728ef80558fa82af3d91eb74c151a838bf6
arvindng88 <arvindng88@gmail.com>
2022-09-20 17:58:27 +0530

    arvindisback

--- data.tf
+++ data.tf
@@ -8,28 +8,28 @@ output "virtual_machine_id" {
 }
 
 data "azurerm_virtual_machine" "ci2" {
-  name1              = "ci-vm-1"
+  name           = "ci-vm-1"
   resource_group_name = var.resoucegp
 }
 
-output "virtual_machine_id" {
+output "virtual_machine_id-1" {
   value = data.azurerm_virtual_machine.ci2.id
 }
 
 data "azurerm_virtual_machine" "ci3" {
-  name2              = "ci-vm-2"
+  name              = "ci-vm-2"
   resource_group_name = var.resoucegp
 }
 
-output "virtual_machine_id" {
+output "virtual_machine_id-2" {
   value = data.azurerm_virtual_machine.ci3.id
 }
 
 data "azurerm_virtual_machine" "ci4" {
-  name3               = "ci-vm-3"
+  name               = "ci-vm-3"
   resource_group_name = var.resoucegp
 }
 
-output "virtual_machine_id" {
+output "virtual_machine_id-3" {
   value = data.azurerm_virtual_machine.ci4.id
 }
