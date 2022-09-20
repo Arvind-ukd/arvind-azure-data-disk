5d4fcc65781e1ee37c912e4b6fd8eac35c699f9a
arvindng88 <arvindng88@gmail.com>
2022-09-20 18:13:08 +0530

    balu

--- data.tf
+++ data.tf
@@ -1,35 +1,38 @@
 data "azurerm_virtual_machine" "ci1" {
   name               = "ci-vm-0"
+  name1               = "ci-vm-1"
+  name2               = "ci-vm-2"
+  name3               = "ci-vm-3"
   resource_group_name = var.resoucegp
 }
 
-output "virtual_machine_id" {
-  value = data.azurerm_virtual_machine.ci1.id
-}
+#output "virtual_machine_id" {
+ # value = data.azurerm_virtual_machine.ci1.id
+#}
 
-data "azurerm_virtual_machine" "ci2" {
-  name           = "ci-vm-1"
-  resource_group_name = var.resoucegp
-}
+#data "azurerm_virtual_machine" "ci2" {
+ # name           = "ci-vm-1"
+  #resource_group_name = var.resoucegp
+#}
 
-output "virtual_machine_id-1" {
-  value = data.azurerm_virtual_machine.ci2.id
-}
+#output "virtual_machine_id-1" {
+ # value = data.azurerm_virtual_machine.ci2.id
+#}
 
-data "azurerm_virtual_machine" "ci3" {
-  name              = "ci-vm-2"
-  resource_group_name = var.resoucegp
-}
+#data "azurerm_virtual_machine" "ci3" {
+ # name              = "ci-vm-2"
+  #resource_group_name = var.resoucegp
+#}
 
-output "virtual_machine_id-2" {
-  value = data.azurerm_virtual_machine.ci3.id
-}
+#output "virtual_machine_id-2" {
+ # value = data.azurerm_virtual_machine.ci3.id
+#}
 
-data "azurerm_virtual_machine" "ci4" {
-  name               = "ci-vm-3"
-  resource_group_name = var.resoucegp
-}
+#data "azurerm_virtual_machine" "ci4" {
+ # name               = "ci-vm-3"
+  #resource_group_name = var.resoucegp
+#}
 
-output "virtual_machine_id-3" {
-  value = data.azurerm_virtual_machine.ci4.id
-}
+#output "virtual_machine_id-3" {
+ # value = data.azurerm_virtual_machine.ci4.id
+#}
